ab4bc58b623c88de050fbf95bed4df21bdd95c05
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2025-01-17 16:57:41 -0800

    Fixes autoscroll to now and go to date used multiple times in sequence.

--- Sources/CROWTimelineView/TimelineVStack.swift
+++ Sources/CROWTimelineView/TimelineVStack.swift
@@ -218,7 +218,7 @@ public struct TimelineVStack: View {
                             x: unitPointXOffset,
                             y: unitPointYOffset)
                         )
-                        viewModel.scrollOffset.x = offset
+                        viewModel.scrollOffset.x = -offset
                     }
                 }
         }
